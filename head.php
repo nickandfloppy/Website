bd3190e5bea9562f9f2b77e2ea9a63218d8614fa
Frankie B <floppydisk05@aol.com>
2022-04-23 22:14:46 +0000

    Major design update to match other sites

---
+++ head.php
@@ -0,0 +1,6 @@
+<?php
+echo "<link rel=\"stylesheet\" href=\"/src/style.css\">\n";
+echo "    <link rel=\"icon\" type=\"image/png\" href=\"/favicon.png\">\n";
+//if (isset($title)) echo "    <title>NetDrivers / ".$title."</title>\n";
+//else echo "    <title>NetDrivers</title>\n";
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <?php include 'head.php'; ?>
+    <title>nick and floppy's website</title>
+</head>
+
+<body>
+	<a href="/"><table><tr>
+		<td><img src="/favicon.png" width="50"></td>
+		<td><h1 style="margin: 0">nick and floppy</h1><i>IT Tech + Dumbass = ???</i></td>
+	</tr></table></a>
+	<hr>
+    <?php include 'nav.html'; ?>
+    <hr>
+    <p>This page currently has no content. Use the above links to navigate the site!</p>
+	<hr>
+	<address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address>
+</body>
+</html>
+
+</html>
