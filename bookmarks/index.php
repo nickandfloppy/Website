353256118797157cd1e2607b682368410c4ea9ce
Frankie B <floppydisk05@aol.com>
2022-06-26 23:52:50 +0000

    Literally add a single space

--- bookmarks/index.php
+++ bookmarks/index.php
@@ -8,7 +8,7 @@
 	<center><table border="1" bgcolor="3c3c84" width="800px"><tr><td>
     	<?php require('../pageheader.html'); ?>
     	<hr>
-    	<h1>Bookmarks<small>(cool websites)</small></h1>
+    	<h1>Bookmarks <small>(cool websites)</small></h1>
         <table><td><img src="./icons/winworld.png"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
         <p>WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</p>
         <hr>
