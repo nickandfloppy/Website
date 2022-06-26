74773de3f26965bb1b7b91f93f9e911e031a8200
Frankie B <floppydisk05@aol.com>
2022-06-26 23:52:11 +0000

    Update projects page

--- projects/index.php
+++ projects/index.php
@@ -9,16 +9,14 @@
     	<?php require('../pageheader.html'); ?>
     	<hr>
     	<h1>Projects</h1>
-        <table><td><img src="https://git.nickandfloppy.com/repo-avatars/42-e991d8508d9e3cdef5807818f9f40fb8" width="35"></td><td><h2>NetRunner</h2></td></table>
+        <table><td><img src="https://git.nickandfloppy.com/repo-avatars/42-e991d8508d9e3cdef5807818f9f40fb8" width="35"></td><td><h2><a href="https://github.com/nickandfloppy/NetRunner">NetRunner</a></h2></td></table>
         <p>A fork of Starman0620's WinBot, modified for use in <a href="/corner">The Corner</a>.</p>
-        <address><a href="https://github.com/nickandfloppy/NetRUnner">GitHub</a></address>
-        <br>        <table><td><img src="http://drivers.nickandfloppy.com/favicon.png" width="35"></td><td><h2>NetDrivers <small>(<a href="http://drivers.nickandfloppy.com/">HTTP</a> / <a href="https://drivers.nickandfloppy.com/">HTTPS</a>)</small></h2></td></table>
-        <p>A driver archival website</p>
-        <address><a href="https://github.com/nickandfloppy/NetDrivers">GitHub</a></address>
         <br>
-        <table><td><img src="http://rewind.nickandfloppy.com/favicon.png" width="35"></td><td><h2>SoftwareRewind <small>(<a href="http://rewind.nickandfloppy.com/">HTTP</a> / <a href="https://rewind.nickandfloppy.com/">HTTPS</a>)</small></h2></td></table>
+        <table><td><img src="http://drivers.nickandfloppy.com/favicon.png" width="35"></td><td><h2><a href="https://github.com/nickandfloppy/NetDrivers">NetDrivers</a> <small>(<a href="http://drivers.nickandfloppy.com/">HTTP</a> / <a href="https://drivers.nickandfloppy.com/">HTTPS</a>)</small></h2></td></table>
+        <p>A driver archival website</p>
+        <br>
+        <table><td><img src="http://rewind.nickandfloppy.com/favicon.png" width="35"></td><td><h2><a href="https://github.com/nickandfloppy/SoftwareRewind">SoftwareRewind</a> <small>(<a href="http://rewind.nickandfloppy.com/">HTTP</a> / <a href="https://rewind.nickandfloppy.com/">HTTPS</a>)</small></h2></td></table>
 		<p>A software archival website</p>
-        <address><a href="https://github.com/nickandfloppy/SoftwareRewind">GitHub</a></address>
         <hr>
 		<center><address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address></center>
 	</td></tr></table></center>
