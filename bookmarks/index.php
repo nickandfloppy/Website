ea257c11a4dd104720ca4f1d245c10fe680a5699
Frankie B <floppydisk05@aol.com>
2022-06-26 23:52:23 +0000

    Add bookmarks page

---
+++ bookmarks/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <?php require('../head.php'); ?>
+    <title>nick and floppy's bookmarks</title>
+</head>
+
+<body>
+	<center><table border="1" bgcolor="3c3c84" width="800px"><tr><td>
+    	<?php require('../pageheader.html'); ?>
+    	<hr>
+    	<h1>Bookmarks<small>(cool websites)</small></h1>
+        <table><td><img src="./icons/winworld.png"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
+        <p>WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</p>
+        <hr>
+		<center><address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address></center>
+	</td></tr></table></center>
+</body>
+</html>
+
+</html>
