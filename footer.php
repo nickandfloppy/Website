c6fbe86fc2f311d058f0a666416e93cf8081aae0
Frankie B <floppydisk05@aol.com>
2022-06-27 22:33:15 +0000

    Things

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
-<center><address>&copy; <?php echo date('Y'); ?> <a href="http://nickandfloppy.com/">nick and floppy</a></address></center>
+<center>
+            <a href="http://www.nickandfloppy.com/">HTTP</a> / <a href="https://www.nickandfloppy.com/">HTTPS</a>
+            <address>&copy; <?php echo date('Y'); ?> <a href="http://nickandfloppy.com/">nick and floppy</a></address>
+        </center>
