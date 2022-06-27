5366d65e9f10d098de59a5d9cf0c9af4153d0cf3
Frankie B <floppydisk05@aol.com>
2022-06-27 13:40:01 +0000

    Update projects

--- projects/index.php
+++ projects/index.php
@@ -11,7 +11,7 @@
     	<h1>Projects</h1>
         <hr>
         <table><td><img src="https://git.nickandfloppy.com/repo-avatars/42-e991d8508d9e3cdef5807818f9f40fb8" width="35"></td><td><h2><a href="https://github.com/nickandfloppy/NetRunner">NetRunner</a></h2></td></table>
-        <p>A fork of Starman0620's WinBot, modified for use in <a href="/corner">The Corner</a>.</p>
+        <p>A fork of <a href="https://github.com/CamK06">Starman0620</a>'s <a href="https://github.com/CamK06/WinBot">WinBot</a>, modified for use in <a href="/corner">The Corner</a>.</p>
         <br>
         <table><td><img src="http://drivers.nickandfloppy.com/favicon.png" width="35"></td><td><h2><a href="https://github.com/nickandfloppy/NetDrivers">NetDrivers</a> <small>(<a href="http://drivers.nickandfloppy.com/">HTTP</a> / <a href="https://drivers.nickandfloppy.com/">HTTPS</a>)</small></h2></td></table>
         <p>A driver archival website</p>
