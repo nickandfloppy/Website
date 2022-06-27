59d711d5f4741685c42aa22290c81115a04608aa
Frankie B <floppydisk05@aol.com>
2022-06-27 10:14:14 +0000

    Add pages to bookmarks & add email to header

--- bookmarks/index.php
+++ bookmarks/index.php
@@ -10,8 +10,43 @@
     	<hr>
     	<h1>Bookmarks <small>(cool websites)</small></h1>
         <hr>
-        <table><td><img src="./icons/winworld.png"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
+        <br>
+        <h2>Computing &amp; Technology</h2>
+        <hr>
+        <table><td><img src="./icons/winworld.png" title="WinWorldPC icon"></td><td><h2><a href="https://winworldpc.com/home">WinWorldPC</a></h2></td></table>
         <p>WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</p>
+        <br>
+        <h2>Friends' Sites</h2>
+        <hr>
+        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://joshuaalto.com/">Sashi</a></h2></td></table>
+        <p>...lol marquees</p>
+        <br>
+        <table><td><img src="./icons/danielle.png" title="Danielle Wheeler icon"></td><td><h2><a href="https://danika.jukor.net/">Danielle Wheeler</a></h2></td></table>
+        <p></p>
+        <br>
+        <table><td><img src="./icons/strangeness.png" width="32" height="32" title="Strangeness Blog icon"></td><td><h2><a href="http://strangenessblog.net/">Strangeness Blog</a></h2></td></table>
+        <p>noone's blog</p>
+        <br>
+        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="https://campos02.neocities.org/">campos</a></h2></td></table>
+        <p>Cool brazilian dude, does programming and stuff</p>
+        <br>
+        <table><td><img src="./icons/hiden.png" title="HIDEN64 icon"></td><td><h2><a href="https://hiden64.ddns.net/">HIDEN64</a></h2></td></table>
+        <p>Moar buttons!</p>
+        <br>
+        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://cocomark.neocities.org/">Coco's Website</a></h2></td></table>
+        <p>Microsoft Edge 89.0.774.75</p>
+        <br>
+        <h2>Other Cool Stuff</h2>
+        <hr>
+        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://toastytech.com/">Toastytech.com</a></h2></td></table>
+        <p>Nathan's Toasty Technology Page</p>
+        <br>
+        <table><td><img src="./icons/noicon.png" title="No icon available"></td><td><h2><a href="http://www.hoary.org/browse/">Optimized for no one</a></h2></td></table>
+        <p>Optimized for no one, but pretty much OK with . . .</p>
+        <br>
+        <table><td><img src="./icons/cameronsworld.png" title="Cameron's World icon"></td><td><h2><a href="https://www.cameronsworld.net/">Cameron's World</a></h2></td></table>
+        <p>A love letter to the Internet of old.</p>
+        <br>
         <hr>
 		<center><address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address></center>
 	</td></tr></table></center>
